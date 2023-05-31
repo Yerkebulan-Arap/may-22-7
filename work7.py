91dddf009e697611bdb48128baee97b045ef33fd
Yerkebulan <134504390+Yerkebulan-Arap@users.noreply.github.com>
2023-05-31 10:18:29 +0600

    Add files via upload

---
+++ work7.py
@@ -0,0 +1,5 @@
+user_input1 = input("Введите год рождения:")
+user_input2 = input("Введите месяц рождения:")
+user_input3 = input("Введите день рождения:")
+result = user_input3+"."+user_input2+"."+user_input1
+print(result)
